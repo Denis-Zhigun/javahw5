bc5b77faf53c6b5bf4bdc44453f409bb60c6b4c2
Denis Zhigun <zhigundv@gmail.com>
2021-01-10 16:00:53 +0300

    Initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shoudCalculateNotRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected =10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+     assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateNotRegisteredOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 26_000_60;
+        boolean registered = false;
+        long expected =500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
