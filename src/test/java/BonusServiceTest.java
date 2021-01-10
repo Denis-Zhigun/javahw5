8b6af16ff3e4fc43c84b08b1f29e361e382aa9e7
Denis Zhigun <zhigundv@gmail.com>
2021-01-10 18:36:56 +0300

    Update BunusServiceTest

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -11,12 +11,12 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
-        long expected =10;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-     assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,11 +26,43 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 26_000_60;
         boolean registered = false;
-        long expected =500;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void shoudCalculateNotRegisteredAndUnderLimitMax() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shoudCalculateNotRegisteredOverLimitZero() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = false;
+        long expected = 0;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+
 }
