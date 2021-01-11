d975ac2b5a2034ea398b9594c038c6e5cbeb9bbf
Denis Zhigun <zhigundv@gmail.com>
2021-01-11 21:22:55 +0300

    BonusServiceTest for true

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -64,5 +64,36 @@ class BonusServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 100;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 
 }
